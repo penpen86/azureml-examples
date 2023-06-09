8ffb0aac544b372ddc7c6316a4b19964da75c8ea
pgurazada <pavankumar.gurazada@gmail.com>
2023-06-09 11:47:56 +0530

    creating a minimal pipeline workflow

--- sdk/python/assets/data/src/read_data.py
+++
@@ -1,9 +0,0 @@
-import argparse
-import pandas as pd
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--input_data", type=str)
-args = parser.parse_args()
-
-df = pd.read_csv(args.input_data)
-print(df.head(10))
